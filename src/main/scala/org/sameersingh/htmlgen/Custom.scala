224cf34ae00c8fc0a00fa6827be0d543a4ba46e0
Sameer Singh <sameeersingh@gmail.com>
2014-12-07 22:00:30 -0500

    more graph apis

--- src/main/scala/org/sameersingh/htmlgen/Custom.scala
+++ src/main/scala/org/sameersingh/htmlgen/Custom.scala
@@ -22,6 +22,15 @@ object Custom {
   case class Edge[B](source: Int, target: Int, description: B, value: Double = 1.0, group: Int = 0)
   case class Graph[A,B](nodes: Seq[Node[A]], edges: Seq[Edge[B]], directed: Boolean = false)
 
+  object Graph {
+    def simple(nodes: Seq[String], edges: Seq[(Int, Int)]): Graph[String, (Int,Int)] =
+      Graph(nodes.map(n => Node(n, n)), edges.map(e => Edge(e._1, e._2, e._1 -> e._2)))
+    def nodeDesc[A](nodes: Seq[(String, A)], edges: Seq[(Int, Int)]): Graph[A, (Int,Int)] =
+      Graph(nodes.map(na => Node(na._1, na._2)), edges.map(e => Edge(e._1, e._2, e._1 -> e._2)))
+    def apply[A,B](nodes: Seq[(String, A)], edges: Seq[(Int, Int, B)]): Graph[A, B] =
+      Graph(nodes.map(na => Node(na._1, na._2)), edges.map(e => Edge(e._1, e._2, e._3)))
+  }
+
   case class Animation[A](frames: Seq[(String, A)])
 
 }
